a2a9098905beb4c3a92da2d7cc133883acb2fd07
Nash Junior <nashjuniorjj@gmail.com>
2024-01-20 20:41:42 -0300

    feat: :sparkles: add update, delete, and find by id coordinate

--- src/@core/src/clients/infra/repositories/postgresql/clients-coordinates-repository.ts
+++ src/@core/src/clients/infra/repositories/postgresql/clients-coordinates-repository.ts
@@ -1,7 +1,8 @@
-import Database from '#clients/infra/db/connection';
+import { Database } from '#clients/infra/db';
 import { ClientCoordinate } from '#clients/domain/entities/client-coordinates';
 import { ClientsCoordinatesRepository } from '#clients/domain/repository';
 import { InvalidUUIDError, UniqueEntityId } from '#seedwork/domain';
+import { Client } from '#clients/domain';
 
 export class ClientCoordinatesRepository
   implements ClientsCoordinatesRepository.Repository
@@ -11,11 +12,58 @@ export class ClientCoordinatesRepository
   ): Promise<ClientsCoordinatesRepository.SearchResult> {
     throw new Error('Method not implemented.');
   }
-  createBatch(instance: ClientCoordinate[]): Promise<void> {
-    throw new Error('Method not implemented.');
+  async createBatch(instances: ClientCoordinate[]): Promise<void> {
+    let parametersNumbers = '';
+    const parameters: any[] = [];
+
+    for (let index = 0; index < instances.length; index++) {
+      const element = instances[index];
+
+      //generating parameters indexes
+      parametersNumbers = parametersNumbers.concat(
+        `(
+        $${(index + 1) * 7 - 6},
+        $${(index + 1) * 7 - 5},
+        $${(index + 1) * 7 - 4},
+        $${(index + 1) * 7 - 3},
+        $${(index + 1) * 7 - 2},
+        $${(index + 1) * 7 - 1},
+        $${(index + 1) * 7}
+      )`,
+      );
+
+      if (index !== instances.length - 1)
+        parametersNumbers = parametersNumbers.concat(',');
+
+      parameters.push(
+        element.uuid,
+        element.client.uuid,
+        element.latitude,
+        element.longitude,
+        element.createdAt,
+        null,
+        false,
+      );
+    }
+
+    const query = `
+    INSERT INTO clients_coordinates (id, client_id, latitude, longitude, created_at, updated_at, deleted)
+    VALUES ${parametersNumbers}
+  `;
+
+    await Database.getInstance().query(query, parameters);
   }
-  delete(id: string | UniqueEntityId): Promise<void> {
-    throw new Error('Method not implemented.');
+
+  async delete(id: string | UniqueEntityId): Promise<void> {
+    const query = `
+    UPDATE clients_coordinates
+    SET deleted = true, updated_at = $2
+    WHERE id = $1 and deleted = false
+  `;
+
+    const parameters = [typeof id === 'string' ? id : id.value, new Date()];
+
+    await Database.getInstance().query(query, parameters);
   }
   find(): Promise<ClientCoordinate[]> {
     throw new Error('Method not implemented.');
@@ -25,14 +73,17 @@ export class ClientCoordinatesRepository
   }
   async create(clientCoordinate: ClientCoordinate): Promise<void> {
     const query = `
-      INSERT INTO clients_coordinates (id, client_id, latitude, longitude)
-      VALUES ($1, $2, $3, $4)
+      INSERT INTO clients_coordinates (id, client_id, latitude, longitude, created_at, updated_at, deleted)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
     `;
     const values = [
       clientCoordinate.uuid,
       clientCoordinate.client.uuid,
       clientCoordinate.latitude,
       clientCoordinate.longitude,
+      clientCoordinate.createdAt,
+      null,
+      false,
     ];
     await Database.getInstance().query(query, values);
   }
@@ -40,31 +91,55 @@ export class ClientCoordinatesRepository
   async update(clientCoordinate: ClientCoordinate): Promise<void> {
     const query = `
       UPDATE clients_coordinates
-      SET latitude = $2, longitude = $3
+      SET latitude = $2, longitude = $3, updated_at = $4
       WHERE id = $1
     `;
     const values = [
-      clientCoordinate.id.value,
+      clientCoordinate.uuid,
       clientCoordinate.latitude,
       clientCoordinate.longitude,
+      new Date(),
     ];
     await Database.getInstance().query(query, values);
   }
 
   async findById(id: UniqueEntityId | string): Promise<ClientCoordinate> {
-    const query = `SELECT * FROM clients_coordinates WHERE id = $1`;
+    const query = `SELECT
+    cc.*,
+    c.name,
+    c.email,
+    c.phone_number,
+    c.deleted as deleted_client,
+    c.created_at as created_at_client,
+    c.updated_at as updated_at_client
+    FROM clients_coordinates cc
+      join clients c on cc.client_id = c.id
+    WHERE cc.id = $1 and cc.deleted = false`;
     const values = [typeof id === 'string' ? id : id.value];
     const response = await Database.getInstance().query(query, values);
-    if (response.rows.length === 0) {
-      throw new InvalidUUIDError();
-    }
+    if (response.rows.length === 0) throw new InvalidUUIDError();
+
     const row = response.rows[0];
-    return ClientCoordinate.create(
+
+    const client = await Client.create(
+      { name: row.name, email: row.email, phoneNumber: row.phone_number },
+      new UniqueEntityId(row.client_id),
       {
-        latitude: row.latitude,
-        longitude: row.longitude,
+        createdAt: row.created_at_client,
+        updatedAt: row.updated_at_client,
+        deleted: row.deleted_client,
       },
+    );
+
+    return ClientCoordinate.create(
+      client,
+      { latitude: row.latitude, longitude: row.longitude },
       new UniqueEntityId(row.id),
+      {
+        createdAt: row.created_at,
+        deleted: row.deleted,
+        updatedAt: row.updated_at,
+      },
     );
   }
 
