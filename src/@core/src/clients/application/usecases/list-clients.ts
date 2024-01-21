f3e623fcaf03723718b381aac8326c6f68f0f6c4
Nash Junior <nashjuniorjj@gmail.com>
2024-01-21 15:15:24 -0300

    refactor: :recycle: fix reponse type list clients

--- src/@core/src/clients/application/usecases/list-clients.ts
+++ src/@core/src/clients/application/usecases/list-clients.ts
@@ -3,9 +3,7 @@ import { ClientsRepository } from '#clients/domain';
 export class Usecase {
   constructor(private readonly usersRepository: ClientsRepository.Repository) {}
 
-  async execute(
-    data: ClientsRepository.SearchParams,
-  ): Promise<ClientsRepository.SearchResult> {
-    return this.usersRepository.search(data);
+  async execute(data: ClientsRepository.SearchParams): Promise<unknown> {
+    return (await this.usersRepository.search(data)).toJSON();
   }
 }
