1fcebfff8e526c36472764d70ac16758d55e465f
Nash Junior <nashjuniorjj@gmail.com>
2024-01-20 23:57:17 -0300

    feat: :construction: add search in params filter

--- src/@core/src/clients/infra/repositories/postgresql/clients-coordinates-repository.ts
+++ src/@core/src/clients/infra/repositories/postgresql/clients-coordinates-repository.ts
@@ -7,11 +7,113 @@ import { Client } from '#clients/domain';
 export class ClientCoordinatesRepository
   implements ClientsCoordinatesRepository.Repository
 {
-  search(
+  protected applyFilter(
+    filter: ClientsCoordinatesRepository.Filter | null,
+  ): string {
+    if (filter == null) return '';
+
+    const queryParams = filter.fields
+      .map(
+        (f, index) => `($${index + 1} ILIKE $${
+          (filter?.fields.length ?? 1) + 1
+        })
+    `,
+      )
+      .join(' OR ');
+
+    return ` AND (${queryParams})`;
+  }
+
+  protected applyPagination(
+    filter: ClientsCoordinatesRepository.SearchParams,
+  ): string {
+    return ` OFFSET ${filter.page * filter.perPage - filter.perPage} LIMIT ${
+      filter.perPage
+    }`;
+  }
+
+  protected applySort(
+    filter: ClientsCoordinatesRepository.SearchParams,
+  ): string {
+    if (filter.orderSort == null || filter.sort == null) return '';
+    //feito condicionar apenas por causa da tipagem, poderia incluir em um único if
+    if (
+      filter.orderSort.length < 1 ||
+      filter.sort.length < 1 ||
+      filter.orderSort.length !== filter.sort.length
+    )
+      return '';
+
+    const mappedOrderBy: string = filter.sort
+      .map((f, index) => ` ${f} ${filter.orderSort?.[index]}`)
+      .join(',');
+
+    return ` ORDER BY ${mappedOrderBy}`;
+  }
+
+  async search(
     props: ClientsCoordinatesRepository.SearchParams,
   ): Promise<ClientsCoordinatesRepository.SearchResult> {
-    throw new Error('Method not implemented.');
+    const baseQueryJoinClients = `
+    FROM clients_coordinates cc
+      join clients c on cc.client_id = c.id
+    WHERE cc.deleted = false`;
+
+    const queryFindItems = `
+    SELECT
+    cc.*,
+    c.name,
+    c.email,
+    c.phone_number,
+    c.deleted as deleted_client,
+    c.created_at as created_at_client,
+    c.updated_at as updated_at_client
+    ${baseQueryJoinClients}
+    `;
+    const queryCountItems = `SELECT COUNT(cc.*) ${baseQueryJoinClients}`;
+
+    const stringApplyFilter = this.applyFilter(props._filter);
+    const stringApplySort = this.applySort(props);
+    const stringApplyPagination = this.applyPagination(props);
+
+    const finalQuerySelectAll = queryFindItems
+      .concat(stringApplyFilter)
+      .concat(stringApplySort)
+      .concat(stringApplyPagination);
+
+    const finalQueryCountAll = queryCountItems
+      .concat(stringApplyFilter)
+      .concat(stringApplyPagination);
+
+    const paramsQuery = [
+      ...Object.values(props._filter?.fields ?? []),
+      props._filter?.query,
+    ];
+
+    console.log(finalQuerySelectAll);
+
+    const [response] = await Promise.all([
+      Database.getInstance().query(
+        finalQuerySelectAll,
+        paramsQuery.length > 0 ? paramsQuery : undefined,
+      ),
+      Database.getInstance().query(
+        finalQueryCountAll,
+        paramsQuery.length > 0 ? paramsQuery : undefined,
+      ),
+    ]);
+
+    return new ClientsCoordinatesRepository.SearchResult({
+      items: [],
+      total: 1,
+      filter: props.filter,
+      sort: props.sort,
+      orderSort: props.orderSort,
+      currentPage: props.page,
+      perPage: props.perPage,
+    });
   }
+
   async createBatch(instances: ClientCoordinate[]): Promise<void> {
     let parametersNumbers = '';
     const parameters: any[] = [];
@@ -65,8 +167,46 @@ export class ClientCoordinatesRepository
 
     await Database.getInstance().query(query, parameters);
   }
-  find(): Promise<ClientCoordinate[]> {
-    throw new Error('Method not implemented.');
+
+  async find(): Promise<ClientCoordinate[]> {
+    const query = `SELECT
+    cc.*,
+    c.name,
+    c.email,
+    c.phone_number,
+    c.deleted as deleted_client,
+    c.created_at as created_at_client,
+    c.updated_at as updated_at_client
+    FROM clients_coordinates cc
+      join clients c on cc.client_id = c.id
+    WHERE cc.deleted = false and c.deleted = false`;
+
+    const response = await Database.getInstance().query(query);
+
+    return Promise.all(
+      response.rows.map(async row => {
+        const client = await Client.create(
+          { name: row.name, email: row.email, phoneNumber: row.phone_number },
+          new UniqueEntityId(row.client_id),
+          {
+            createdAt: row.created_at_client,
+            updatedAt: row.updated_at_client,
+            deleted: row.deleted_client,
+          },
+        );
+
+        return ClientCoordinate.create(
+          client,
+          { latitude: row.latitude, longitude: row.longitude },
+          new UniqueEntityId(row.id),
+          {
+            createdAt: row.created_at,
+            deleted: row.deleted,
+            updatedAt: row.updated_at,
+          },
+        );
+      }),
+    );
   }
   findAndCount(): Promise<{ total: number; items: ClientCoordinate[] }> {
     throw new Error('Method not implemented.');
