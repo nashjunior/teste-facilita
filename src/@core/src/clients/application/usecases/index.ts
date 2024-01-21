e4b88b4d5c7a8e87d66ecc37798a078b13062543
Nash Junior <nashjuniorjj@gmail.com>
2024-01-21 10:29:50 -0300

    feat: :sparkles: add usecase generate user routes

--- src/@core/src/clients/application/usecases/index.ts
+++ src/@core/src/clients/application/usecases/index.ts
@@ -2,3 +2,4 @@ export * as CreateClientUsecase from './create-client';
 export * as FindClientUsecase from './find-client';
 export * as DeleteClientUsecase from './delete-client';
 export * as UpdateClientUsecase from './update-client';
+export * as GenerateRouteUsecase from './generate-routes';
