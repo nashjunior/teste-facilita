195d4ca692e347374d56a0a0ab3059dfd2cffc6e
Nash Junior <nashjuniorjj@gmail.com>
2024-01-21 18:41:49 -0300

    fix: :bug: fix db hostname

--- src/@core/src/clients/infra/config/index.ts
+++ src/@core/src/clients/infra/config/index.ts
@@ -2,7 +2,7 @@ import enviroment from './enviroment';
 
 export default {
   user: enviroment.user,
-  host: enviroment.database,
+  host: enviroment.host,
   database: enviroment.database,
   password: enviroment.password,
   port: enviroment.port,
