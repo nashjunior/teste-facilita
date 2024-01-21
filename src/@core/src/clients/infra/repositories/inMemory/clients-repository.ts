b8bee6ed400bb6ad242f299796afcc7387ab4948
Nash Junior <nashjuniorjj@gmail.com>
2024-01-20 23:51:33 -0300

    feat: :art: clients in memory repository implements find by email

--- src/@core/src/clients/infra/repositories/inMemory/clients-repository.ts
+++ src/@core/src/clients/infra/repositories/inMemory/clients-repository.ts
@@ -1,4 +1,5 @@
 import { Client } from '#clients/domain/entities';
+import { EmailAlreadyExistentError } from '#clients/domain/errors/email-already-existent';
 import { ClientsRepository } from '#clients/domain/repository';
 import { InMemorySearchbleRepository } from '#seedwork/domain/repository';
 
@@ -6,6 +7,10 @@ export class ClientsInMemoryRepository
   extends InMemorySearchbleRepository<Client>
   implements ClientsRepository.Repository
 {
+  async findByEmail(email: string): Promise<void> {
+    if (this.items.find(item => item.email === email && item.deleted === false))
+      throw new EmailAlreadyExistentError(email);
+  }
   protected async applyFilter(
     items: Client[],
     filter: ClientsRepository.Filter,
