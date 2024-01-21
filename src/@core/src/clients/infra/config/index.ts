8f3811a4cab9da1487c04d1cb8529399c7d95470
Nash Junior <nashjuniorjj@gmail.com>
2024-01-21 17:46:21 -0300

    feat: :truck: add exports

---
+++ src/@core/src/clients/infra/config/index.ts
@@ -0,0 +1,9 @@
+import enviroment from './enviroment';
+
+export default {
+  user: enviroment.user,
+  host: enviroment.database,
+  database: enviroment.database,
+  password: enviroment.password,
+  port: enviroment.port,
+};
--- src/@core/src/clients/infra/index.ts
+++ src/@core/src/clients/infra/index.ts
@@ -1,2 +1,3 @@
 export * from './repositories';
 export * from './db';
+export * from './config';
