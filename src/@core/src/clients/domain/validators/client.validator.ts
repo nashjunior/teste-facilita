ffddeb6b950c78d740dc35bc129fb877240007bb
Nash Junior <nashjuniorjj@gmail.com>
2024-01-21 14:09:16 -0300

    refactor: :recycle: add min character validation in field client validator

--- src/@core/src/clients/domain/validators/client.validator.ts
+++ src/@core/src/clients/domain/validators/client.validator.ts
@@ -10,7 +10,7 @@ export class ClientValidator extends YupValidator<IClientProps> {
         .shape({
           name: yup.string().required().max(255),
           email: yup.string().email().max(255),
-          phoneNumber: yup.string().max(20),
+          phoneNumber: yup.string().min(10).max(20),
         })
         .required()
         .noUnknown(),
