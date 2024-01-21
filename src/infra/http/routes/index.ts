47bac5bd6506e7da483a6d13b8b3b018afde373f
Nash Junior <nashjuniorjj@gmail.com>
2024-01-21 17:10:45 -0300

    feat: :art: add exports

--- src/infra/http/routes/index.ts
+++ src/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { clientsRoutes } from "./clients";
 import { clientsCoordinatesRoutes } from "./clients-coordinates";
+import { generateRouteRoutes } from "./route";
 
 export const generateRoutes = (fastify : FastifyInstance ) => {
   fastify.register(clientsRoutes, {prefix: '/clients'})
   fastify.register(clientsCoordinatesRoutes, {prefix: '/clients-coordinates'})
+  fastify.register(generateRouteRoutes, {prefix: '/routes'})
 
 }
