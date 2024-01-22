f225ded8f40f7f53ee0ab166bdad98b653442d73
Nash Junior <nashjuniorjj@gmail.com>
2024-01-22 09:13:49 -0300

    chore: :label: add output client coordinate type

---
+++ src/@core/src/clients/application/dto/client-coordinate-output.dto.ts
@@ -0,0 +1,9 @@
+export type IClientCoordinateOutput = {
+  id: string;
+  clientId: string;
+  latitude: string;
+  longitude: string;
+  createdAt: Date;
+  updatedAt: Date | null;
+  deleted: boolean;
+};
